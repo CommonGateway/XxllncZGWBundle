697310125361f811fc2e9120866283a0a1ed4e09
Barry Brands <barrybrands02@hotmail.com>
2023-02-13 14:15:19 +0100

    ZaakTypeCommand added id argument

--- Command/XxllncToZGWZaakTypeCommand.php
+++ Command/XxllncToZGWZaakTypeCommand.php
@@ -8,6 +8,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Console\Input\InputArgument;
 
 /**
  * Command to execute the FindOrganizationThroughRepositoriesService.
@@ -27,13 +29,24 @@ class XxllncToZGWZaakTypeCommand extends Command
     {
         $this
             ->setDescription('This command triggers Xxllnc xxllncToZGWZaakTypeService')
-            ->setHelp('This command triggers Xxllnc xxllncToZGWZaakTypeService');
+            ->setHelp('This command triggers Xxllnc xxllncToZGWZaakTypeService')
+            ->addArgument('id', InputArgument::OPTIONAL, 'Casetype id to fetch from xxllnc');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
         $this->xxllncToZGWZaakTypeService->setStyle($io);
+        $id = $input->getArgument('id');
+
+        if (isset($id) && Uuid::isValid($id)) {
+            $io->info('ID is valid, trying to fetch and map casetype '.$id.' to a ZGW ZaakType');
+            if (!$this->xxllncToZGWZaakTypeService->getZaakType($id)) {
+                return Command::FAILURE;
+            }
+            
+            return Command::SUCCESS;
+        }
 
         if (!$this->xxllncToZGWZaakTypeService->xxllncToZGWZaakTypeHandler()) {
             return Command::FAILURE;
