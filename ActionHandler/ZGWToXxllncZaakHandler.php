58ef2c16772c5fb042aa9febabe9ac43b88d10c1
Barry Brands <barrybrands02@hotmail.com>
2023-02-24 16:14:26 +0100

    handler cleanup

--- ActionHandler/ZGWToXxllncZaakHandler.php
+++ ActionHandler/ZGWToXxllncZaakHandler.php
@@ -28,20 +28,10 @@ class ZGWToXxllncZaakHandler implements ActionHandlerInterface
         return [
             '$id'         => 'https://example.com/person.schema.json',
             '$schema'     => 'https://json-schema.org/draft/2020-12/schema',
-            'title'       => 'Zaakeigenschappen Action',
-            'description' => 'This handler posts zaak eigenschappen from ZDS to ZGW',
-            'required'    => ['identifierPath'],
-            'properties'  => [
-                'identifierPath' => [
-                    'type'        => 'string',
-                    'description' => 'The DNS of the mail provider, see https://symfony.com/doc/6.2/mailer.html for details',
-                    'example'     => 'native://default',
-                ],
-                'eigenschappen' => [
-                    'type'        => 'array',
-                    'description' => '',
-                ],
-            ],
+            'title'       => 'ZGWZaakToXxllnc',
+            'description' => 'This handler posts zgw zaak to xxllnc',
+            'required'    => [],
+            'properties'  => [],
         ];
     }
 
