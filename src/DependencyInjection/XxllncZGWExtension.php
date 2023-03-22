4a4f8cb23a2eb6bc0685af17c46baf9a043c0961
Robert Zondervan <robert@conduction.nl>
2023-03-22 14:02:41 +0100

    use /../../src

--- src/DependencyInjection/XxllncZGWExtension.php
+++ src/DependencyInjection/XxllncZGWExtension.php
@@ -20,7 +20,7 @@ class XxllncZGWExtension extends Extension
 
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../Resources/config'));
         $loader->load('services.yaml');
 
     }//end load()
