ef9f8237fdaaec2f884ff5267a529e138d7e7491
Wilco Louwerse <wilco@conduction.nl>
2023-04-04 16:11:01 +0200

    Final cleanup translations in installationService

--- src/Service/InstallationService.php
+++ src/Service/InstallationService.php
@@ -4,12 +4,10 @@
  *
  * By creating symfony objects that function as configuration for the gateway.
  *
- * @author Conduction BV <info@conduction.nl>, Barry Brands <barry@conduction.nl>
+ * @author Conduction BV <info@conduction.nl>, Barry Brands <barry@conduction.nl>, Wilco Louwerse <wilco@conduction.nl>
  * @license EUPL-1.2 https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
  *
  * @category Service
- *
- * @todo update to new installation method (check zgwbundle as example)
  */
 
 // src/Service/InstallationService.php
@@ -20,7 +18,7 @@ use App\Entity\Translation;
 use CommonGateway\CoreBundle\Installer\InstallerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectRepository;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Psr\Log\LoggerInterface;
 
 
 class InstallationService implements InstallerInterface
@@ -30,77 +28,84 @@ class InstallationService implements InstallerInterface
      * @var EntityManagerInterface
      */
     private EntityManagerInterface $entityManager;
-
+    
     /**
-     * @var SymfonyStyle
+     * The installation logger.
+     *
+     * @var LoggerInterface
      */
-    private SymfonyStyle $style;
-
-    /**
-     * @var ObjectRepository
-     */
-    private ObjectRepository $schemaRepository;
-
-    /**
-     * @var ObjectRepository
-     */
-    private ObjectRepository $attributeRepository;
-
+    private LoggerInterface $logger;
+    
     /**
      * @var ObjectRepository
      */
     private ObjectRepository $translationRepository;
-
-
+    
+    private const TRANSLATIONS = [
+        ["translateFrom" => "Nee", "translateTo" => false],
+        ["translateFrom" => "Ja", "translateTo" => true],
+        ["translateFrom" => "internextern", "translateTo" => "intern"],
+        ["translateFrom" => "Vernietigen (V)", "translateTo" => "vernietigen"],
+        ["translateFrom" => "Bewaren (B)", "translateTo" => "blijvend_bewaren"],
+    ];
+    
+    
     /**
-     * __construct.
+     * The constructor
+     *
+     * @param EntityManagerInterface $entityManager The entity manager.
+     * @param LoggerInterface $installationLogger The installation logger.
      */
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        LoggerInterface $installationLogger
+    ) {
         $this->entityManager = $entityManager;
-
-        $this->schemaRepository      = $this->entityManager->getRepository('App:Entity');
-        $this->attributeRepository   = $this->entityManager->getRepository('App:Attribute');
+        $this->logger = $installationLogger;
+    
         $this->translationRepository = $this->entityManager->getRepository('App:Translation');
-
+        
     }//end __construct()
-
-
+    
+    
     /**
-     * Set symfony style in order to output to the console.
+     * Every installation service should implement an install function
      *
-     * @param SymfonyStyle $style
-     *
-     * @return self
-     *
-     * @todo change to monolog
+     * @return void
      */
-    public function setStyle(SymfonyStyle $style): self
-    {
-        $this->style = $style;
-
-        return $this;
-
-    }//end setStyle()
-
-
     public function install()
     {
+        $this->logger->debug("XxllncZGWBundle -> Install()");
+        
         $this->checkDataConsistency();
 
     }//end install()
-
-
+    
+    
+    /**
+     * Every installation service should implement an update function
+     *
+     * @return void
+     */
     public function update()
     {
+        $this->logger->debug("XxllncZGWBundle -> Update()");
+        
         $this->checkDataConsistency();
 
     }//end update()
-
-
+    
+    
+    /**
+     * Every installation service should implement an uninstall function
+     *
+     * @return void
+     */
     public function uninstall()
     {
-        // Do some cleanup
+        $this->logger->debug("XxllncZGWBundle -> Uninstall()");
+    
+        // Do some cleanup to uninstall correctly...
 
     }//end uninstall()
 
@@ -112,19 +117,8 @@ class InstallationService implements InstallerInterface
      */
     public function checkDataConsistency(): void
     {
-        // Return if zgw is not installed.
-        if ($this->isZGWBundleInstalled() == false) {
-            return;
-        }
-
-        // Get xllncZaakPost schema.
-        $xxllncZaakPost = $this->schemaRepository->findOneBy(['name' => 'XxllncZaakPost']);
-
         // Update attributes.
-        $this->updateAttributes($xxllncZaakPost);
-
-        // Update zgw zaak endpoint to throw event that gets the zaak to xxllnc.
-        $this->updateZGWZaakEndpoint($xxllncZaakPost);
+        $this->updateAttributes();
 
         // Translations.
         $this->createTranslations();
@@ -135,60 +129,35 @@ class InstallationService implements InstallerInterface
 
 
     /**
-     * Checks if ZGWBundle is installed.
-     *
-     * If not we cant install this XxllncZGWBundle.
-     *
-     * @return bool true if installed, false if not
-     */
-    private function isZGWBundleInstalled()
-    {
-        $ZGWZaak = $this->schemaRepository->findOneBy(['reference' => 'https://vng.opencatalogi.nl/schemas/zrc.zaak.schema.json']);
-        if ($ZGWZaak === null) {
-            isset($this->style) === true && $this->style->error('ZGWBundle not installed, please make sure that bundle is installed before this one');
-
-            return false;
-        }
-
-        isset($this->style) === true && $this->style->info('ZGWBundle is installed, continueing..');
-
-        return true;
-
-    }//end isZGWBundleInstalled()
-
-
-    /**
      * Updates attributes to simple array attributes.
      *
-     * @param Entity|null $xxllncZaakPost Schema.
-     *
      * @return void
      */
-    private function updateAttributes(?Entity $xxllncZaakPost): void
+    private function updateAttributes(): void
     {
-        if ($xxllncZaakPostFiles = $this->attributeRepository->findOneBy(['entity' => $xxllncZaakPost, 'name' => 'files'])) {
-            $xxllncZaakPostFiles->setObject(null);
-            $xxllncZaakPostFiles->setMultiple(false);
-            $xxllncZaakPostFiles->setType('array');
-            $this->entityManager->persist($xxllncZaakPostFiles);
-        }//end if
+        // Get xllncZaakPost Schema.
+        $reference = 'https://development.zaaksysteem.nl/schema/xxllnc.zaakPost.schema.json';
+        $xxllncZaakPost = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $reference]);
+        if ($xxllncZaakPost === null) {
+            $this->logger->error("No entity found for $reference.", ['plugin' => 'common-gateway/xxllnc-zgw-bundle']);
+            
+            return;
+        }
+        
+        // Get xllncZaakPost -> files Attribute.
+        $xxllncZaakPostFiles = $this->entityManager->getRepository('App:Attribute')->findOneBy(['entity' => $xxllncZaakPost, 'name' => 'files']);
+        if ($xxllncZaakPostFiles === null) {
+            $this->logger->error("No attribute 'files' found for entity: $reference.", ['plugin' => 'common-gateway/xxllnc-zgw-bundle']);
+            
+            return;
+        }
+    
+        $xxllncZaakPostFiles->setObject(null);
+        $xxllncZaakPostFiles->setMultiple(false);
+        $xxllncZaakPostFiles->setType('array');
+        $this->entityManager->persist($xxllncZaakPostFiles);
 
     }//end updateAttributes()
-
-
-    /**
-     * Updates ZGW Zaak endpoint with a throw event.
-     *
-     * @return void
-     */
-    private function updateZGWZaakEndpoint(): void
-    {
-        isset($this->style) === true && $this->style->writeln(['', '<info>Updating zgw zaak endpoint</info>']);
-        $endpoint = $this->entityManager->getRepository('App:Endpoint')->findOneBy(['name' => 'Zaak']);
-        $endpoint->setThrows(['zgw.zaak.saved']);
-        $this->entityManager->persist($endpoint);
-
-    }//end updateZGWZaakEndpoint()
 
 
     /**
@@ -196,50 +165,44 @@ class InstallationService implements InstallerInterface
      *
      * @return void
      *
-     * @todo Should be integrated in mapping
+     * @todo Should be integrated in mapping->translations.
      */
     private function createTranslations(): void
     {
-        isset($this->style) === true && $this->style->writeln(['', '<info>Looking for translations</info>']);
-        $trans = ($this->translationRepository->findOneBy(['translateFrom' => 'Nee', 'translationTable' => 'caseTypeTable1']) ?? new Translation());
-        $trans->setTranslationTable('caseTypeTable1');
-        $trans->setTranslateFrom('Nee');
-        $trans->setTranslateTo(false);
-        $trans->setLanguage('nl');
-        $this->entityManager->persist($trans);
-        isset($this->style) === true && $this->style->writeln('Translation created');
-
-        $trans = ($this->translationRepository->findOneBy(['translateFrom' => 'Ja', 'translationTable' => 'caseTypeTable1']) ?? new Translation());
-        $trans->setTranslationTable('caseTypeTable1');
-        $trans->setTranslateFrom('Ja');
-        $trans->setTranslateTo(true);
-        $trans->setLanguage('nl');
-        $this->entityManager->persist($trans);
-        isset($this->style) === true && $this->style->writeln('Translation created');
-
-        $trans = ($this->translationRepository->findOneBy(['translateFrom' => 'internextern', 'translationTable' => 'caseTypeTable1']) ?? new Translation());
-        $trans->setTranslationTable('caseTypeTable1');
-        $trans->setTranslateFrom('internextern');
-        $trans->setTranslateTo('intern');
-        $trans->setLanguage('nl');
-        $this->entityManager->persist($trans);
-        isset($this->style) === true && $this->style->writeln('Translation created');
-
-        $trans = ($this->translationRepository->findOneBy(['translateFrom' => 'Vernietigen (V)', 'translationTable' => 'caseTypeTable1']) ?? new Translation());
-        $trans->setTranslationTable('caseTypeTable1');
-        $trans->setTranslateFrom('Vernietigen (V)');
-        $trans->setTranslateTo('vernietigen');
-        $trans->setLanguage('nl');
-        $this->entityManager->persist($trans);
-        isset($this->style) === true && $this->style->writeln('Translation created');
-
-        $trans = ($this->translationRepository->findOneBy(['translateFrom' => 'Bewaren (B)', 'translationTable' => 'caseTypeTable1']) ?? new Translation());
-        $trans->setTranslationTable('caseTypeTable1');
-        $trans->setTranslateFrom('Bewaren (B)');
-        $trans->setTranslateTo('blijvend_bewaren');
-        $trans->setLanguage('nl');
-        $this->entityManager->persist($trans);
-        isset($this->style) === true && $this->style->writeln('Translation created');
+        $this->logger->info("Looking for translations");
+        
+        foreach ($this::TRANSLATIONS as $translation) {
+            $translation['translationTable'] = 'caseTypeTable1';
+            $translation['language'] = 'nl';
+            $this->createTranslation($translation);
+        }
 
     }//end createTranslations()
+    
+    /**
+     * Create a single translation needed for xxllnc to zgw.
+     *
+     * @param array $data An array with data, used to check if a Translation already exists, else creating it with this data.
+     *
+     * @return void
+     */
+    private function createTranslation(array $data): void
+    {
+        $translation = $this->translationRepository->findOneBy(['translateFrom' => $data['translateFrom'], 'translationTable' => $data['translationTable']]);
+        if ($translation !== null) {
+            $this->logger->debug("Translation found", ['id' => $translation->getId()->toString()]);
+            
+            return;
+        }
+        
+        $translation = new Translation();
+        $translation->setTranslationTable($data['translationTable']);
+        $translation->setTranslateFrom($data['translateFrom']);
+        $translation->setTranslateTo($data['translateTo']);
+        $translation->setLanguage($data['language']);
+        $this->entityManager->persist($translation);
+        
+        $this->logger->debug("Translation created", ['id' => $translation->getId()->toString()]);
+        
+    }//end createTranslation()
 }//end class
